80d67760ec201889774000ae6f69e45d73c23195
JCuzzy1 <cousins.joseph@googlemail.com>
2024-05-05 15:27:57 +0100

    Ran the frst test in 03_reverseString and passed! Learned how to reverse a string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const word = "" // create variable - empty string
+const splitWord = word.split("") // split our string into an array of letters
+const reverseWord = splitWord.reverse() // reverse order of items in our array
+const result = reverseWord.join("") // join the items in our array back into a string
 
-};
+function reverseString(str) { // chanin methods together to a nice function
+    return str
+        .split("")
+        .reverse()
+        .join("")
+
+}
 
 // Do not edit below this line
 module.exports = reverseString;
