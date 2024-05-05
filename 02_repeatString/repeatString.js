db30350c834acea13c8a33339c6966c646e5a349
JCuzzy1 <cousins.joseph@googlemail.com>
2024-05-05 13:55:58 +0100

    Running the other tests for exercise 02_repeatString was a struggle. Had to look at multiple solutions to understand the consept. The if statement to return an error for a negative number was easy to get. I declared an  empty string, called it repeatedString. Then ran a for loop: let i=0 (index starting at zero); i<num (condition: as long as i is less than the number or iterations); i++ (increment the value by 1). I then added the string to my empty string (repeatedString += string) and then returned repeatedString. Everything passed. I need more practice, however!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -7,12 +7,38 @@ This function will take two arguments, string and num.
 There are multple tests in this exercise (see file repeatString.spec.js).
 Only the first test is currently enabled. So after making sure that this
 first one passes, enable the others one by one by deleting the .skip from
-the test.skip() function. */
+the test.skip() function.*/
 
-const repeatString = function(string,num) {
-    return string.repeat(num)
+/* const repeatString = function(string,num) { //two arguments: string and num
+    return string.repeat(num) //return string a given number of times
 
 };
+
+// Do not edit below this line
+module.exports = repeatString; */
+
+/* First test (above) passed.
+
+Second test: Repeat 'hello' 10 times.
+Third test: Repeat 'hi' 1 time.
+Fourth test: Repeat 'bye' 0 times.
+Fifth test: Repeat 'goodbye' -1 times - expected to return an error. */
+
+
+const repeatString = function(string,num) { 
+    let repeatedString = "";
+    if(num < 0) {
+        return 'ERROR';
+    }
+    for(let i = 0; i < num; i++) {
+        repeatedString += string; // repeatedString = repeatedString + string
+    }
+
+    return repeatedString;
+
+};
+
+// console.log(repeatString(repeatedString, num));
 
 // Do not edit below this line
 module.exports = repeatString;
