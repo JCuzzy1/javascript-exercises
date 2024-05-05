eddab55f507556a31879e96bcc410e7bdad4de0a
JCuzzy1 <cousins.joseph@googlemail.com>
2024-05-05 11:08:13 +0100

    Ran the first test for exercise 02_repeatString and passed. Took a few attempts because I put repeat.string(num) instead of string.repeat(num).

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+/* Write a function that simply repeats the string a given number of times:
+
+repeatString('hey', 3) // returns 'heyheyhey'
+
+This function will take two arguments, string and num.
+
+There are multple tests in this exercise (see file repeatString.spec.js).
+Only the first test is currently enabled. So after making sure that this
+first one passes, enable the others one by one by deleting the .skip from
+the test.skip() function. */
+
+const repeatString = function(string,num) {
+    return string.repeat(num)
 
 };
 
